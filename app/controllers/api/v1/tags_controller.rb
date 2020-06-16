62dd56037ec33f64123dcc9eee5ad36253e42349
winnerdev2012 <winner.dev202@gmail.com>
2020-06-15 22:38:46 -0400

    Add tag

---
+++ app/controllers/api/v1/tags_controller.rb
@@ -0,0 +1,42 @@
+class Api::V1::TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :update, :destroy]
+  
+  def index
+    @tags = Tag.all
+    render json: @tags
+  end
+
+  def show
+    render json: @tag
+  end
+
+  def create
+    if @tag.create(tag_params)
+      render json: @tag
+    else
+      render json: {message: "Tag not created."}
+    end
+  end
+
+  def update
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: {message: "Tag not updated."}
+    end
+  end
+  
+  def destroy
+    render json: @tag
+  end
+  
+  private
+  def tag_params
+    params.require(:tag).permit(:name, :description)
+  end
+  
+  def set_tag
+    @tag = Tag.find(params[:id])
+  end
+end
+  
---
+++ app/serializers/tag_serializer.rb
@@ -0,0 +1,15 @@
+class TagSerializer < ActiveModel::Serializer
+  attributes :id, :name
+  has_many :taggings
+  has_many :boards, through: :taggings
+  has_many :resources, through: :taggings
+  has_many :diets, through: :taggings
+  has_many :meals, through: :taggings
+  has_many :recipes, through: :taggings
+  has_many :documents, through: :taggings
+  has_many :medications, through: :taggings
+  has_many :playlists, through: :taggings
+  has_many :schedules, through: :taggings
+  has_many :treatments, through: :taggings
+  has_many :users, through: :taggings
+end
