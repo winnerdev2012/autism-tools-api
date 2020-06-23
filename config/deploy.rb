95e6cbb31dc7d64d96e2fbb658fbfdc5ccfd85ef
winnerdev2012 <winner.dev202@gmail.com>
2020-06-22 21:53:10 -0400

    Remove master.key requirement

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,8 +23,7 @@ set :pty, true
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml", "config/master.key"
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/master.key"
-
+append :linked_files, "config/database.yml", "config/secrets.yml"
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
