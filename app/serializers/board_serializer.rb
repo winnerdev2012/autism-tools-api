400f2b55294fef1660ce012bdf9a97cb8d3bdf52
winnerdev2012 <winner.dev202@gmail.com>
2020-08-22 12:46:58 -0400

    Add board_resources and taggings to board_serializer

--- app/serializers/board_serializer.rb
+++ app/serializers/board_serializer.rb
@@ -1,5 +1,5 @@
 class BoardSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description
+  attributes :id, :name, :description, :board_resources, :taggings
   has_many :board_resources
   has_many :resources, through: :board_resources
   has_many :taggings
