add2c2bac4fa12cefc97c9dc3eb8dd3e83012047
winnerdev2012 <winner.dev202@gmail.com>
2020-08-22 12:41:00 -0400

    Add dependent destroy to board_resources and taggings

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,6 @@
 class Board < ApplicationRecord
-  has_many :board_resources
+  has_many :board_resources, dependent: :destroy
   has_many :resources, through: :board_resources
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings, source: :board
 end
